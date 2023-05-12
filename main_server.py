6f91996768532e6954e69d1c86f84ad9eb873868
Christian Fleury <chfleurysiq@gmail.com>
2023-05-11 22:07:18 -0300

    feat: add interface to mainserver

--- main_server.py
+++ main_server.py
@@ -1,18 +1,58 @@
 import socket
 import json
+import threading
+import os
+import time
 
 HOST = "localhost"
 PORT = 10582
 
-sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP IPV4
-sckt.bind((HOST, PORT))
-sckt.listen()
-print("Waiting for client...")
-connection, address = sckt.accept()
+firstFloorMap = [False] * 8
+secondFloorMap = [False] * 8
 
-while True:
-    data = connection.recv(512)
-    print("data", json.loads(data.decode()))
-    x = json.loads(data.decode())
-    print(x["test"])
-    connection.sendall(data)
+socketInstance = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP IPV4
+socketInstance.bind((HOST, PORT))
+socketInstance.listen()
+
+
+def handleSocketCommunication():
+    while True:
+        print("Waiting for client...")
+        connection, address = socketInstance.accept()
+
+        while True:
+            try:
+                data = connection.recv(64).decode()
+
+                dataDictionary = json.loads(data)
+
+                if dataDictionary["metadata"] == "secondFloor":
+                    secondFloorMap = dataDictionary["parkingSpacesMap"]
+                elif dataDictionary["metadata"] == "firstFloor":
+                    firstFloorMap = dataDictionary["parkingSpacesMap"]
+
+                print("data", json.loads(data))
+                x = json.loads(data.decode())
+                print(x["test"])
+
+                connection.sendall(data)
+            except:
+                break
+
+
+def userInterface():
+    print("Primeiro Andar:", firstFloorMap)
+    print("Segundo Andar:", secondFloorMap)
+    time.sleep(0.3)  # TODO
+    os.system("clear")
+
+
+def sendMessage(data) -> None:
+    try:
+        socketInstance.send(str.encode(json.dumps(data)))  # TODO
+    except:
+        pass
+
+
+threading.Thread(target=handleSocketCommunication).start()
+threading.Thread(target=userInterface).start()
--- second_floor.py
+++ second_floor.py
@@ -2,23 +2,36 @@ import RPi.GPIO as GPIO
 import time
 import threading
 import socket
+import json
+
+HOST = "localhost"
+PORT = 10582
+
+socketInstance = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP IPV4
 
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(13, GPIO.OUT)  # ENDERECO_01
 GPIO.setup(06, GPIO.OUT)  # ENDERECO_02
 GPIO.setup(05, GPIO.OUT)  # ENDERECO_03
 GPIO.setup(20, GPIO.IN)  # SENSOR_DE_VAGA
-GPIO.setup(8, GPIO.OUT)  # SINAL_DE_LOTADO_FECHADO
+GPIO.setup(08, GPIO.OUT)  # SINAL_DE_LOTADO_FECHADO
 GPIO.setup(16, GPIO.IN)  # SENSOR_DE_PASSAGEM_1
 GPIO.setup(21, GPIO.IN)  # SENSOR_DE_PASSAGEM_2
 
 
-def readParkingSpaces():
+def sendMessage(data) -> None:
+    data['metadata'] = 'secondFloor'
+    try:
+        socketInstance.send(str.encode(json.dumps(data))) # TODO
+    except:
+        pass
+
+
+def readParkingSpaces() -> None:
     parkingSpacesMap = [False] * 8
     while True:
         for i in range(8):
-            time.sleep(0.03)
+            time.sleep(0.03) # TODO VERIFY NEED OF THIS SLEEP
 
             GPIO.output(13, i & 1)
             GPIO.output(06, (i & 2) >> 1)
@@ -31,8 +44,12 @@ def readParkingSpaces():
             parkingSpacesMap[i] = isParkingSpaceBusy
 
         print("Second Floor Parking Spaces: ", parkingSpacesMap)
+       
+        sendMessage({
+            "parkingSpacesMap": parkingSpacesMap
+        })
 
-def handleSecondFloorEntrance():
+def handleSecondFloorEntrance() -> None:
     sensorOneTime = False
     sensorTwoTime = False
 
@@ -59,4 +76,20 @@ def handleSecondFloorEntrance():
                 sensorTwoTime = False
             else: time.sleep(0.1)
 
-threading.Thread(target=readParkingSpaces).start()
+def handleSocketCommunication() -> None:
+    while True:
+        socketInstance.connect((HOST, PORT))
+
+        while True:
+            try:
+                data = socketInstance.recv(64)
+                print("data", json.loads(data.decode()))
+            except:
+                break
+
+
+def main():
+    threading.Thread(target=handleSocketCommunication).start()
+    threading.Thread(target=readParkingSpaces).start()
+
+main()
