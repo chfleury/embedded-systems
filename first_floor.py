f54b4a6819a9bb778e20c77079313085aa0e8e8e
Christian Fleury <chfleurysiq@gmail.com>
2023-05-14 00:20:06 -0300

    uncomment gpio stuff

--- first_floor.py
+++ first_floor.py
@@ -1,4 +1,4 @@
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 import threading
 import socket
@@ -15,70 +15,65 @@ parkingSpacesMap = {
     "metadata": "firstFloor"
 }
 
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setwarnings(False)
-# GPIO.setup(22, GPIO.OUT)  # ENDERECO_01
-# GPIO.setup(26, GPIO.OUT)  # ENDERECO_02
-# GPIO.setup(19, GPIO.OUT)  # ENDERECO_03
-# GPIO.setup(18, GPIO.IN)  # SENSOR_DE_VAGA
-# GPIO.setup(27, GPIO.OUT)  # SINAL_DE_LOTADO_FECHADO
-# GPIO.setup(23, GPIO.IN)  # SENSOR_ABERTURA_CANCELA_ENTRADA
-# GPIO.setup(24, GPIO.IN)  # SENSOR_FECHAMENTO_CANCELA_ENTRADA
-# GPIO.setup(10, GPIO.OUT)  # MOTOR_CANCELA_ENTRADA
-# GPIO.setup(25, GPIO.IN)  # SENSOR_ABERTURA_CANCELA_SAIDA
-# GPIO.setup(12, GPIO.IN)  # SENSOR_FECHAMENTO_CANCELA_SAIDA
-# GPIO.setup(17, GPIO.OUT)  # MOTOR_CANCELA_SAIDA
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(22, GPIO.OUT)  # ENDERECO_01
+GPIO.setup(26, GPIO.OUT)  # ENDERECO_02
+GPIO.setup(19, GPIO.OUT)  # ENDERECO_03
+GPIO.setup(18, GPIO.IN)  # SENSOR_DE_VAGA
+GPIO.setup(27, GPIO.OUT)  # SINAL_DE_LOTADO_FECHADO
+GPIO.setup(23, GPIO.IN)  # SENSOR_ABERTURA_CANCELA_ENTRADA
+GPIO.setup(24, GPIO.IN)  # SENSOR_FECHAMENTO_CANCELA_ENTRADA
+GPIO.setup(10, GPIO.OUT)  # MOTOR_CANCELA_ENTRADA
+GPIO.setup(25, GPIO.IN)  # SENSOR_ABERTURA_CANCELA_SAIDA
+GPIO.setup(12, GPIO.IN)  # SENSOR_FECHAMENTO_CANCELA_SAIDA
+GPIO.setup(17, GPIO.OUT)  # MOTOR_CANCELA_SAIDA
 
 
 def readParkingSpaces():
     while True:
         for i in range(8):
-            # GPIO.output(22, i & 1)
-            # GPIO.output(26, (i & 2) >> 1)
-            # GPIO.output(19, (i & 4) >> 2)
+            GPIO.output(22, i & 1)
+            GPIO.output(26, (i & 2) >> 1)
+            GPIO.output(19, (i & 4) >> 2)
 
             time.sleep(0.05)
 
-            isParkingSpaceBusy = False
-
-            if i == 0:
-                isParkingSpaceBusy = True
-
-            # isParkingSpaceBusy = bool(GPIO.input(18))
+            isParkingSpaceBusy = bool(GPIO.input(18))
 
             parkingSpacesMap['parkingSpacesMap'][i] = isParkingSpaceBusy
 
         # print("First Floor Parking Spaces: ", parkingSpacesMap['parkingSpacesMap'])
 
 
-# def handleEntranceParkingBarrier():
-#     while True:
-#         time.sleep(0.05) # TODO
-#         SENSOR_ABERTURA_CANCELA_ENTRADA = GPIO.input(23)
+def handleEntranceParkingBarrier():
+    while True:
+        time.sleep(0.05) # TODO
+        SENSOR_ABERTURA_CANCELA_ENTRADA = GPIO.input(23)
 
-#         if SENSOR_ABERTURA_CANCELA_ENTRADA:
-#             GPIO.output(10, 1)
+        if SENSOR_ABERTURA_CANCELA_ENTRADA:
+            GPIO.output(10, 1)
 
-#             while True:
-#                 SENSOR_FECHAMENTO_CANCELA_ENTRADA = GPIO.input(24)
-#                 if SENSOR_FECHAMENTO_CANCELA_ENTRADA:
-#                     GPIO.output(10, 0)
-#                     break
+            while True:
+                SENSOR_FECHAMENTO_CANCELA_ENTRADA = GPIO.input(24)
+                if SENSOR_FECHAMENTO_CANCELA_ENTRADA:
+                    GPIO.output(10, 0)
+                    break
 
 
-# def handleExitParkingBarrier():
-#     while True:
-#         SENSOR_ABERTURA_CANCELA_SAIDA = GPIO.input(25)
+def handleExitParkingBarrier():
+    while True:
+        SENSOR_ABERTURA_CANCELA_SAIDA = GPIO.input(25)
 
-#         if SENSOR_ABERTURA_CANCELA_SAIDA:
-#             GPIO.output(17, 1)
+        if SENSOR_ABERTURA_CANCELA_SAIDA:
+            GPIO.output(17, 1)
 
-#             while True:
-#                 SENSOR_FECHAMENTO_CANCELA_SAIDA = GPIO.input(12)
-#                 if SENSOR_FECHAMENTO_CANCELA_SAIDA:
-#                     GPIO.output(17, 0)
-#                     break
-#         time.sleep(0.05) # TODO
+            while True:
+                SENSOR_FECHAMENTO_CANCELA_SAIDA = GPIO.input(12)
+                if SENSOR_FECHAMENTO_CANCELA_SAIDA:
+                    GPIO.output(17, 0)
+                    break
+        time.sleep(0.05) # TODO
 
 
 def handleSocketCommunication():
@@ -123,10 +118,10 @@ def handleSocketCommunication():
 
 def setFullFloorLedOn():
     print('entrou aqui pra setar')
-    # GPIO.output(27, 1)
+    GPIO.output(27, 1)
 
 threading.Thread(target=readParkingSpaces).start()
-# threading.Thread(target=handleEntranceParkingBarrier).start()
-# threading.Thread(target=handleExitParkingBarrier).start()
+threading.Thread(target=handleEntranceParkingBarrier).start()
+threading.Thread(target=handleExitParkingBarrier).start()
 threading.Thread(target=handleSocketCommunication).start()
 
--- second_floor.py
+++ second_floor.py
@@ -1,4 +1,4 @@
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 import threading
 import socket
@@ -17,75 +17,69 @@ parkingSpacesMap = {
 }
 
 
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setwarnings(False)
-# GPIO.setup(13, GPIO.OUT)  # ENDERECO_01
-# GPIO.setup(6, GPIO.OUT)  # ENDERECO_02
-# GPIO.setup(5, GPIO.OUT)  # ENDERECO_03
-# GPIO.setup(20, GPIO.IN)  # SENSOR_DE_VAGA
-# GPIO.setup(8, GPIO.OUT)  # SINAL_DE_LOTADO_FECHADO
-# GPIO.setup(16, GPIO.IN)  # SENSOR_DE_PASSAGEM_1
-# GPIO.setup(21, GPIO.IN)  # SENSOR_DE_PASSAGEM_2
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(13, GPIO.OUT)  # ENDERECO_01
+GPIO.setup(6, GPIO.OUT)  # ENDERECO_02
+GPIO.setup(5, GPIO.OUT)  # ENDERECO_03
+GPIO.setup(20, GPIO.IN)  # SENSOR_DE_VAGA
+GPIO.setup(8, GPIO.OUT)  # SINAL_DE_LOTADO_FECHADO
+GPIO.setup(16, GPIO.IN)  # SENSOR_DE_PASSAGEM_1
+GPIO.setup(21, GPIO.IN)  # SENSOR_DE_PASSAGEM_2
 
 
 def readParkingSpaces():
      while True:
         for i in range(8):
 
-            # GPIO.output(13, i & 1)
-            # GPIO.output(6, (i & 2) >> 1)
-            # GPIO.output(5, (i & 4) >> 2)
+            GPIO.output(13, i & 1)
+            GPIO.output(6, (i & 2) >> 1)
+            GPIO.output(5, (i & 4) >> 2)
 
             time.sleep(0.05)
 
-            isParkingSpaceBusy = False
-
-            if i == 2:
-                isParkingSpaceBusy = True
-
-
-            # isParkingSpaceBusy = bool(GPIO.input(20))
+            isParkingSpaceBusy = bool(GPIO.input(20))
 
             parkingSpacesMap['parkingSpacesMap'][i] = isParkingSpaceBusy
  
 
-# def handleSecondFloorEntrance():
-#     sensorOneTime = None
-#     sensorTwoTime = None
+def handleSecondFloorEntrance():
+    sensorOneTime = None
+    sensorTwoTime = None
 
-#     while True:
-#         SENSOR_DE_PASSAGEM_1 = bool(GPIO.input(16))
-#         SENSOR_DE_PASSAGEM_2 = bool(GPIO.input(21))
-#         if sensorOneTime is not None and sensorTwoTime is not None:
+    while True:
+        SENSOR_DE_PASSAGEM_1 = bool(GPIO.input(16))
+        SENSOR_DE_PASSAGEM_2 = bool(GPIO.input(21))
+        if sensorOneTime is not None and sensorTwoTime is not None:
 
-#             print(sensorOneTime, sensorTwoTime)
+            print(sensorOneTime, sensorTwoTime)
 
-#         if SENSOR_DE_PASSAGEM_1 and sensorOneTime is None:
-#             sensorOneTime = time.time()
-#             while bool(GPIO.input(16)):
-#                 pass
-#             # time.sleep(0.3)
+        if SENSOR_DE_PASSAGEM_1 and sensorOneTime is None:
+            sensorOneTime = time.time()
+            while bool(GPIO.input(16)):
+                pass
+            # time.sleep(0.3)
 
-#         if SENSOR_DE_PASSAGEM_2 and sensorTwoTime is None:
-#             sensorTwoTime = time.time()
-#             while bool(GPIO.input(21)):
-#                 pass
+        if SENSOR_DE_PASSAGEM_2 and sensorTwoTime is None:
+            sensorTwoTime = time.time()
+            while bool(GPIO.input(21)):
+                pass
 
-#         if sensorOneTime is not None and sensorTwoTime is not None:
-#             deltaTime = sensorTwoTime - sensorOneTime
+        if sensorOneTime is not None and sensorTwoTime is not None:
+            deltaTime = sensorTwoTime - sensorOneTime
 
-#             if deltaTime > 0:
-#                  print('entrou para o segundo andar!')
-#                  print(time.time())
-#             else: 
-#                 print('saiu para o primeiro andar2!')
-#                 print(time.time())
-#             sensorOneTime = None
-#             sensorTwoTime = None
-#             time.sleep(0.4)
+            if deltaTime > 0:
+                 print('entrou para o segundo andar!')
+                 print(time.time())
+            else: 
+                print('saiu para o primeiro andar2!')
+                print(time.time())
+            sensorOneTime = None
+            sensorTwoTime = None
+            time.sleep(0.4)
 
-#             continue
-#         time.sleep(0.25)
+            continue
+        time.sleep(0.25)
 
 
 
@@ -136,10 +130,10 @@ def handleSocketCommunication():
 
 def setFullFloorLedOn():
     print('led on')
-    # GPIO.output(8, 1)
+    GPIO.output(8, 1)
     pass
 
 threading.Thread(target=handleSocketCommunication).start()
 threading.Thread(target=readParkingSpaces).start()
-# threading.Thread(target=handleSecondFloorEntrance).start()
+threading.Thread(target=handleSecondFloorEntrance).start()
  
