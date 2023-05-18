2438c0e25d879dc1e39d3b0cff1ca1d200214a4d
Christian Fleury <chfleurysiq@gmail.com>
2023-05-17 21:49:10 -0300

    checkpoint

--- uart/first_exercice/src/main.c
+++ uart/first_exercice/src/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>  //Used for UART
 #include <fcntl.h>   //Used for UART
 #include <termios.h> //Used for UART
+#define TAMANHO_MATRICULA 4
 
 int main(int argc, const char *argv[])
 {
@@ -26,21 +28,94 @@ int main(int argc, const char *argv[])
     tcflush(uart0_filestream, TCIFLUSH);
     tcsetattr(uart0_filestream, TCSANOW, &options);
 
-    unsigned char tx_buffer[20];
-    unsigned char *p_tx_buffer;
-
-    p_tx_buffer = &tx_buffer[0];
-    *p_tx_buffer++ = 'H';
-    *p_tx_buffer++ = 'e';
-    *p_tx_buffer++ = 'l';
-    *p_tx_buffer++ = 'l';
-    *p_tx_buffer++ = 'o';
-
-    printf("Buffers de memória criados!\n");
-
-    if (uart0_filestream != -1)
+    unsigned int command;
+    while (command)
     {
+        printf("Digite o comando: ");
+        scanf("%x", &command);
+
+        unsigned int matricula[TAMANHO_MATRICULA] = {1, 6, 0, 2};
+        unsigned char tx_buffer[260];
+        unsigned char *p_tx_buffer;
+
+        p_tx_buffer = &tx_buffer[0];
+
+        memcpy(p_tx_buffer++, &command, 1);
+
+        if (command >= 0xB1 && command <= 0xB3)
+        {
+
+            switch (command)
+            {
+            case 0xB1:
+            {
+                int data;
+                printf("Digite o inteiro a ser enviado.\n");
+
+                scanf("%d", &data);
+
+                memcpy(p_tx_buffer, &data, 1);
+                p_tx_buffer += sizeof(data);
+
+                break;
+            }
+
+            case 0xB2:
+            {
+                float data;
+                printf("Digite o float a ser enviado.\n");
+
+                scanf("%f", &data);
+
+                memcpy(p_tx_buffer, &data, 1);
+                p_tx_buffer += sizeof(data);
+
+                break;
+            }
+            case 0xB3:
+            {
+                printf("Digite o tamanho da string a ser enviada.\n");
+                int size;
+                scanf("%d", &size);
+
+                printf("Digite a string a ser enviada.\n");
+                char data[size];
+                scanf("%s", data);
+
+                memcpy(p_tx_buffer, &size, 1);
+                p_tx_buffer += sizeof(size);
+
+                for (int i = 0; i < size; i++)
+                {
+
+                    *p_tx_buffer = data[i];
+                    p_tx_buffer++;
+                }
+
+                break;
+            }
+
+            default:
+                printf("Comando inválido.\n");
+                continue;
+            }
+        }
+
+        for (int i = 0; i < TAMANHO_MATRICULA; i++)
+        {
+
+            memcpy(p_tx_buffer++, &matricula[i], 1);
+        }
+
+        printf("Buffers de memória criados!\n");
+
         printf("Escrevendo caracteres na UART ...");
+
+        printf("%d\n", tx_buffer[0]);
+        int x;
+        memcpy(&x, &tx_buffer[1], 4);
+        printf("o valode x é %d", x);
+
         int count = write(uart0_filestream, &tx_buffer[0], (p_tx_buffer - &tx_buffer[0]));
         if (count < 0)
         {
@@ -48,18 +123,15 @@ int main(int argc, const char *argv[])
         }
         else
         {
-            printf("escrito.\n");
+            printf("escrito.%d\n", count);
         }
-    }
 
-    sleep(1);
+        sleep(1);
 
-    //----- CHECK FOR ANY RX BYTES -----
-    if (uart0_filestream != -1)
-    {
+        //----- CHECK FOR ANY RX BYTES -----
         // Read up to 255 characters from the port if they are there
-        unsigned char rx_buffer[256];
-        int rx_length = read(uart0_filestream, (void *)rx_buffer, 255); // Filestream, buffer to store in, number of bytes to read (max)
+        unsigned char rx_buffer[257];
+        int rx_length = read(uart0_filestream, (void *)rx_buffer, 256); // Filestream, buffer to store in, number of bytes to read (max)
         if (rx_length < 0)
         {
             printf("Erro na leitura.\n"); // An error occured (will occur if there are no bytes)
@@ -70,9 +142,34 @@ int main(int argc, const char *argv[])
         }
         else
         {
-            // Bytes received
-            rx_buffer[rx_length] = '\0';
-            printf("%i Bytes lidos : %s\n", rx_length, rx_buffer);
+
+            if (command >= 0xA1 && command <= 0xB1)
+            {
+                int data;
+                memcpy(&data, rx_buffer, sizeof(data));
+
+                printf("%i Bytes lidos : %d\n", rx_length, data);
+            }
+            else if (command >= 0xA2 && command <= 0xB2)
+            {
+                float data;
+                memcpy(&data, rx_buffer, sizeof(data));
+
+                printf("%i Bytes lidos : %f\n", rx_length, data);
+            }
+            else if (command >= 0xA3 && command <= 0xB3)
+            {
+
+                int dataSize;
+                memcpy(&dataSize, rx_buffer, 1);
+
+                char data[dataSize + 1];
+                memcpy(&data, &rx_buffer[1], dataSize);
+
+                data[dataSize] = '\0';
+
+                printf("%i Bytes lidos : %s\n", rx_length, data);
+            }
         }
     }
 
